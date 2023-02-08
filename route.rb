5032779ef77be472915d9265a9bf0baa4a8ffe3f
ryohanyu <81959656+ryohanyu@users.noreply.github.com>
2023-02-08 15:50:18 +0900

    Create route.rb

---
+++ route.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  get 'top'=>"home#top"
+  get 'about'=>"home#about"
+  get 'about2'=>"home#about2"
+  get 'about3'=>"home#about3"
+  get 'about4'=>"home#about4"
+  get 'about5'=>"home#about5"
+  get 'about6'=>"home#about6"
+  get 'about7'=>"home#about7"
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
